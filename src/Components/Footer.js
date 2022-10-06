35b81aa4028c69554a18491a6873dca550c23b58
Stephanie Quick <stephaniequick1997@gmail.com>
2022-10-05 23:37:59 -0500

    init commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,25 @@
+import { Container, Row, Col } from "react-bootstrap";
+import navIcon1 from "../assets/images/nav-icon1.svg";
+import navIcon2 from "../assets/images/nav-icon2.svg";
+import navIcon3 from "../assets/images/nav-icon3.svg";
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          <Col size={12} sm={6}>
+          </Col>
+          <Col size={12} sm={6} className="text-center text-sm-end">
+            <div className="social-icon">
+              <a href="https://www.linkedin.com/in/stephanie-quick-55b015212/" target="_blank"><img src={navIcon1} alt="Linkedin" /></a>
+              <a href="#"><img src={navIcon2} alt="Icon" /></a>
+              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+            </div>
+            <p>Copyright 2022. All Rights Reserved</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
